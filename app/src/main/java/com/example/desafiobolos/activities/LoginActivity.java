d60da2ba634f4aa7214aae4cd3b6e06ddd533099
Bruno Gomide <brunoggomide@gmail.com>
2022-10-09 23:50:48 -0300

    telas de acompanhar pedidos

--- app/src/main/java/com/example/desafiobolos/activities/LoginActivity.java
+++ app/src/main/java/com/example/desafiobolos/activities/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+                Intent intent = new Intent(LoginActivity.this, RegistrerActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/desafiobolos/activities/RegistrerActivity.java
+++ app/src/main/java/com/example/desafiobolos/activities/RegistrerActivity.java
@@ -1,19 +1,17 @@
 package com.example.desafiobolos.activities;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.example.desafiobolos.R;
 
-public class CadastroActivity extends AppCompatActivity {
+public class RegistrerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro);
+        setContentView(R.layout.activity_registrer);
 
         getSupportActionBar().hide();
     }
