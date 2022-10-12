3f8f1b71c53458831ebff8bf56050ac8d09b1e14
Bruno Gomide <brunoggomide@gmail.com>
2022-10-12 17:28:31 -0300

    telas finais

--- app/src/main/java/com/example/desafiobolos/activities/MainActivity.java
+++ app/src/main/java/com/example/desafiobolos/activities/MainActivity.java
@@ -1,16 +1,46 @@
 package com.example.desafiobolos.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.desafiobolos.R;
+import com.example.desafiobolos.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    break;
+                case R.id.basket:
+                    break;
+                case R.id.delivery:
+                    break;
+                case R.id.profile:
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
